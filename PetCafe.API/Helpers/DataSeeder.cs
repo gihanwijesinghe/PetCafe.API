f194324dc918c6cffa2c968dbd32a27a697e7495
Gihan <gihanwijesinghe12@gmail.com>
2023-08-12 23:03:52 +0530

    seed data

---
+++ PetCafe.API/Helpers/DataSeeder.cs
@@ -0,0 +1,80 @@
+﻿using PetCafe.API.Data;
+
+namespace PetCafe.API.Helpers
+{
+    public class DataSeeder
+    {
+        public static async Task SeedCafes(CafeDbContext context)
+        {
+            if (!context.Cafes.Any())
+            {
+                var countries = new List<Cafe>
+                {
+                    new Cafe { 
+                        Id = Guid.NewGuid(), 
+                        Name = "Sizzle", 
+                        Description = "European food", 
+                        Location = "Colombo03",
+                        Employees = new List<Employee>
+                        {
+                            new Employee
+                            {
+                                Id = "UI1234567",
+                                Name = "Gihan",
+                                Email = "gihan@gmail.com",
+                                Phone = 81234567,
+                                EmployeeGender = EmployeeGender.Male
+                            }
+                        }
+                    },
+                    new Cafe {
+                        Id = Guid.NewGuid(),
+                        Name = "MCDonald",
+                        Description = "Burgers and food",
+                        Location = "Colombo03",
+                        Employees = new List<Employee>
+                        {
+                            new Employee
+                            {
+                                Id = "UI1234568",
+                                Name = "Pradeep",
+                                Email = "pradeep@gmail.com",
+                                Phone = 81234568,
+                                EmployeeGender = EmployeeGender.Male
+                            },
+                            new Employee
+                            {
+                                Id = "UI1234569",
+                                Name = "Nuwan Sampath",
+                                Email = "nuwan@gmail.com",
+                                Phone = 81234569,
+                                EmployeeGender = EmployeeGender.Male
+                            }
+                        }
+                    },
+                        
+                    new Cafe { 
+                        Id = Guid.NewGuid(),
+                        Name = "PizzaHut", 
+                        Description = "Pizzas and other", 
+                        Location = "Colombo04",
+                        Employees = new List<Employee>
+                        {
+                            new Employee
+                            {
+                                Id = "UI1234570",
+                                Name = "Paba",
+                                Email = "paba@gmail.com",
+                                Phone = 81234570,
+                                EmployeeGender = EmployeeGender.Female
+                            }
+                        }
+                    },
+                };
+
+                context.AddRange(countries);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- PetCafe.API/Program.cs
+++ PetCafe.API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PetCafe.API.Data;
+using PetCafe.API.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CafeDbContext>(item => item.UseSqlServer("string"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,4 +29,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await MigrateAsync(app);
+await SeedDataAsync(app);
+
 app.Run();
+
+static async Task MigrateAsync(IHost host)
+{
+    using var scope = host.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<CafeDbContext>();
+    await db.Database.MigrateAsync();
+}
+
+static async Task SeedDataAsync(IHost host)
+{
+    var scope = host.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<CafeDbContext>();
+    await DataSeeder.SeedCafes(context);
+}
